1f2aa48958d2ce08f0477ad49be73795d3be1f73
Ilya Sazonov <igsazonov@ispras.ru>
2025-04-05 00:04:21 +0300

    hw2

--- src/main/scala/ru/otus/module1/DataCollection1/homework/homework1.scala
+++ src/main/scala/ru/otus/module1/DataCollection1/homework/homework1.scala
@@ -1,18 +1,25 @@
 package ru.otus.module1.DataCollection1
-/*
+
+import scala.util.Random
+
 class BallsExperiment {
 
   def isFirstBlackSecondWhite(): Boolean = {
-    ???
+    val list = List(0, 0, 0, 1, 1, 1)
+    val shuffled = Random.shuffle(list)
+    val first = shuffled.head
+    val second = Random.shuffle(shuffled.tail).head
+
+    first == 1 || second == 1
   }
 }
 
 object BallsTest {
   def main(args: Array[String]): Unit = {
     val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
+    val listOfExperiments: List[BallsExperiment] = List.fill(count)(new BallsExperiment)
+    val countOfExperiments = listOfExperiments.map(_.isFirstBlackSecondWhite())
     val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
     println(countOfPositiveExperiments / count)
   }
-}*/
+}
