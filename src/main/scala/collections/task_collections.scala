adc56dab4533a506a293ab269325f50b25a62e8e
Ilya Sazonov <igsazonov@ispras.ru>
2025-04-14 23:51:10 +0300

    hw3

--- src/main/scala/collections/task_collections.scala
+++ src/main/scala/collections/task_collections.scala
@@ -16,7 +16,11 @@ object task_collections {
    *
    * **/
   def capitalizeIgnoringASCII(text: List[String]): List[String] = {
-    List.empty
+    text.zipWithIndex.collect {
+      case (word, 0) => word
+      case (word, _) if isASCIIString(word) => word.toUpperCase
+      case (word, _) => word.toLowerCase
+    }
   }
 
   /**
@@ -29,7 +33,19 @@ object task_collections {
    * HINT: Для всех возможных комбинаций чисел стоит использовать Map
    * **/
   def numbersToNumericString(text: String): String = {
-    ""
+    val digitToWord = Map(
+      '0' -> "zero",
+      '1' -> "one",
+      '2' -> "two",
+      '3' -> "three",
+      '4' -> "four",
+      '5' -> "five",
+      '6' -> "six",
+      '7' -> "seven",
+      '8' -> "eight",
+      '9' -> "nine"
+    )
+    text.map(c => digitToWord.getOrElse(c, c)).mkString
   }
 
   /**
@@ -47,7 +63,7 @@ object task_collections {
    * Реализуйте метод который примет две коллекции (два источника) и вернёт объединенный список уникальный значений
    **/
   def intersectionAuto(dealerOne: Iterable[Auto], dealerTwo: Iterable[Auto]): Iterable[Auto] = {
-    Iterable.empty
+    (dealerOne ++ dealerTwo).toSet
   }
 
   /**
@@ -56,6 +72,6 @@ object task_collections {
    * и вернёт уникальный список машин обслуживающихся в первом дилерском центре и не обслуживающимся во втором
    **/
   def filterAllLeftDealerAutoWithoutRight(dealerOne: Iterable[Auto], dealerTwo: Iterable[Auto]): Iterable[Auto] = {
-    Iterable.empty
+    dealerOne.toSet.diff(dealerTwo.toSet)
   }
 }
